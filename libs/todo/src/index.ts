82591a66641a79ac5c0a177c3835b17f228282fc
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-10-14 20:33:06 -0300

    todo: create new mock

--- libs/todo/src/index.ts
+++ libs/todo/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/todo.mock';
 export * from './lib/todo.model';
---
+++ libs/todo/src/lib/todo.mock.ts
@@ -0,0 +1,3 @@
+import { Todo, TodoStatus } from './todo.model';
+
+export const mockTodos: Todo[] = [{ id: '0', status: TodoStatus.TODO }];
