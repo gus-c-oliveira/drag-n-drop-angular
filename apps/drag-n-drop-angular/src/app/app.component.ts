0a429c2f3ecea72b821f1e0f5e06488f86d6e9be
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-10-24 10:02:18 -0300

    implement drag and drop functionality

--- apps/drag-n-drop-angular/src/app/app.component.ts
+++ apps/drag-n-drop-angular/src/app/app.component.ts
@@ -1,6 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
-import { getNextStatus, Todo, TodoEvent, TodoStatus } from '@gus/todo';
+import {
+  getAllTodoStatus,
+  Todo,
+  TodoEvent,
+  TodoService,
+  TodoStatus,
+} from '@gus/todo';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'gus-root',
@@ -8,22 +19,57 @@ import { getNextStatus, Todo, TodoEvent, TodoStatus } from '@gus/todo';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public todos: Todo[] = [];
+  public allTodoStatus = getAllTodoStatus();
+  public statusListConnections: { [status: string]: string[] } = {};
+  public todosByStatus: { [status: string]: Todo[] } = {};
 
-  public constructor(private http: HttpClient) {
+  public constructor(private todoService: TodoService) {
+    this.initTodosByStatus();
+    this.initStatusListConnections();
     this.fetchTodos();
   }
 
+  private initTodosByStatus() {
+    this.allTodoStatus.forEach((status) => {
+      this.todosByStatus[status] = [];
+    });
+  }
+
+  private initStatusListConnections() {
+    this.allTodoStatus.forEach((status) => {
+      this.statusListConnections[status] = this.allTodoStatus.filter(
+        (item) => item !== status
+      );
+    });
+  }
+
   private fetchTodos() {
-    this.http
-      .get<Todo[]>('/api/todos')
-      .subscribe((todos) => (this.todos = todos));
+    this.todoService
+      .getTodos()
+      .pipe(take(1))
+      .subscribe(
+        (todos) => {
+          todos.forEach((item) => {
+            this.todosByStatus[item.status].push(item);
+          });
+        },
+        (error) => {
+          console.warn('Error loading todos, please refresh page!');
+        }
+      );
   }
 
   public addTodo() {
-    this.http
-      .post('/api/todos', { status: TodoStatus.TODO })
-      .subscribe(() => this.fetchTodos());
+    this.todoService
+      .createTodo()
+      .pipe(take(1))
+      .subscribe(
+        (response) => {
+          const newTodo = response as Todo;
+          this.todosByStatus[newTodo.status].push(newTodo);
+        },
+        (error) => console.warn('Error creating todo, please try again!')
+      );
   }
 
   public handleReduce(event: TodoEvent) {
@@ -35,12 +81,54 @@ export class AppComponent {
   }
 
   private updateTodo(id: string, status: TodoStatus) {
-    this.http
-      .put('/api/todos', { id, status: getNextStatus(status) })
-      .subscribe(() => this.fetchTodos());
+    this.todoService
+      .updateTodoToNextStatus(id, status)
+      .pipe(take(1))
+      .subscribe(
+        (response) => {
+          const updatedTodo = response as Todo;
+          this.todosByStatus[status] = this.todosByStatus[status].filter(
+            (item) => item.id !== id
+          );
+          this.todosByStatus[updatedTodo.status].push(updatedTodo);
+        },
+        (error) => console.log('Error updating todo, please try again!')
+      );
   }
 
-  private deleteTodo(id: string) {
-    this.http.delete(`/api/todos/${id}`).subscribe(() => this.fetchTodos());
+  private deleteTodo(id: string, status: TodoStatus) {
+    this.todoService
+      .deleteTodo(id)
+      .pipe(take(1))
+      .subscribe(
+        (_) => {
+          this.todosByStatus[status] = this.todosByStatus[status].filter(
+            (item) => item.id !== id
+          );
+        },
+        (error) => console.log('Error deleting todo, please try again!')
+      );
+  }
+
+  public itemDropped(event: CdkDragDrop<any>) {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    const id = event.item.element.nativeElement.id;
+    const status = event.container.id as TodoStatus;
+    const previousStatus = event.previousContainer.id as TodoStatus;
+    this.todoService
+      .updateTodo(id, status)
+      .pipe(take(1))
+      .subscribe(
+        (response) => {
+          const updatedTodo = response as Todo;
+          this.todosByStatus[previousStatus] = this.todosByStatus[
+            previousStatus
+          ].filter((item) => item.id !== id);
+          this.todosByStatus[status].push(updatedTodo);
+        },
+        (error) => console.warn('Error updating todo, please try again!')
+      );
   }
 }
