9a73ad48d7f88deb402126a2f29dab5c4dcd0048
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-10-26 19:37:06 -0300

    todo: add title to todo interface

--- libs/todo/src/lib/model/todo.mock.ts
+++ libs/todo/src/lib/model/todo.mock.ts
@@ -1,3 +1,5 @@
 import { Todo, TodoStatus } from './todo.model';
 
-export const mockTodos: Todo[] = [{ id: '0', status: TodoStatus.TODO }];
+export const mockTodos: Todo[] = [
+  { id: '0', status: TodoStatus.TODO, title: 'Create New Todo' },
+];
